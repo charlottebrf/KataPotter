b8c6b2fcae6664e1f2de1323fd840445904d8946
Charlotte Fereday <cbrfereday@gmail.com>
2017-08-26 08:02:41 +0100

    Refactors cashRegister to different functions to separate responsibilities

--- src/potter.js
+++ src/potter.js
@@ -1,12 +1,29 @@
+const normalPrice = 8
+const fivePercentDiscount = 0.8
+const tenPercentDiscount = 2.40
+const twentyPercentDiscount = 6.40
+const twentyFivePercentDiscount = 10
 
 function cashRegister(assortedHPBooks) {
   if (assortedHPBooks.size == 1) {
-    return 8
+    return oneBook()
   } else if (assortedHPBooks.size == 2 && assortedHPBooks.values().next.key != "Harry Potter and the Philosopher's Stone") {
-    return 15.2
+    return twoDifferentBooks()
   } else {
-    return 21.60
+    return threeDifferentBooks()
   }
+}
+
+function oneBook() {
+  return normalPrice
+}
+
+function twoDifferentBooks() {
+  return (normalPrice * 2) - fivePercentDiscount
+}
+
+function threeDifferentBooks() {
+  return (normalPrice * 3) - tenPercentDiscount
 }
 
 module.exports = cashRegister;
--- test/potter.test.js
+++ test/potter.test.js
@@ -10,7 +10,7 @@ test('two different copies of a HP book costs gives a 5% discount', () => {
   expect(cashRegister(assortedHPBooks)).toBe(15.2);
 });
 
-test('two different copies of a HP book costs gives a 5% discount', () => {
+test('two different copies of a HP book costs gives a 10% discount', () => {
     let assortedHPBooks = new Map([['Harry Potter and the Philosopher\'s Stone', 1], ['Second book', 1], ['Third book', 1]]);
   expect(cashRegister(assortedHPBooks)).toBe(21.60);
 });
