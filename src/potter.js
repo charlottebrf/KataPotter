3e2dc3e6465edc3be95c7a3e26eba7a4cb621bc9
Charlotte Fereday <cbrfereday@gmail.com>
2017-08-27 23:10:48 +0100

    Calculator works for all 5 discounts, including all possible combinations of books

--- src/potter.js
+++ src/potter.js
@@ -11,12 +11,12 @@ function cashRegister(assortedHPBooks) {
     return oneBook()
   } else if (assortedHPBooks.length == 2) {
     return twoBooks(assortedHPBooks)
-  } else if (assortedHPBooks.length == 3 && allDifferentBooks(assortedHPBooks) == true) {
-    return threeDifferentBooks()
-  }  else if (assortedHPBooks.length == 4 && allDifferentBooks(assortedHPBooks) == true) {
-    return fourDifferentBooks()
-  } else if (assortedHPBooks.length == 5 && allDifferentBooks(assortedHPBooks) == true) {
-    return fiveDifferentBooks()
+  } else if (assortedHPBooks.length == 3) {
+    return threeBooks(assortedHPBooks)
+  }  else if (assortedHPBooks.length == 4) {
+    return fourBooks(assortedHPBooks)
+  } else if (assortedHPBooks.length == 5) {
+    return fiveBooks(assortedHPBooks)
   }
 }
 
@@ -24,16 +24,54 @@ function twoBooks(assortedHPBooks) {
   if (allDifferentBooks(assortedHPBooks) == true) {
     return twoDifferentBooks()
   } else if (duplicativeBooks(assortedHPBooks) == 1) {
-    return 23.2
+    return 2 * oneBook()
+  }
+}
+
+function threeBooks(assortedHPBooks) {
+  if (allDifferentBooks(assortedHPBooks) == true) {
+    return threeDifferentBooks()
+  } else if (duplicativeBooks(assortedHPBooks) == 2) {
+    return oneBook() + twoDifferentBooks()
+  } else {
+    return 3 * oneBook()
+  }
+}
+
+function fourBooks(assortedHPBooks) {
+  if (allDifferentBooks(assortedHPBooks) == true) {
+    return fourDifferentBooks()
+  } else if (duplicativeBooks(assortedHPBooks) == 3) {
+    return oneBook() + threeDifferentBooks()
+  } else if (duplicativeBooks(assortedHPBooks) == 2) {
+    return (2 * oneBook()) + twoDifferentBooks()
+  } else {
+    return 4 * oneBook()
+  }
+}
+
+function fiveBooks(assortedHPBooks) {
+  if (allDifferentBooks(assortedHPBooks) == true) {
+    return fiveDifferentBooks()
+  } else if (duplicativeBooks(assortedHPBooks) == 4) {
+    return oneBook() + fourDifferentBooks()
+  } else if (duplicativeBooks(assortedHPBooks) == 3) {
+    return (2 * oneBook()) + threeDifferentBooks()
+  } else if (duplicativeBooks(assortedHPBooks) == 2){
+    return (3 * oneBook()) + twoDifferentBooks()
+  } else {
+    return 5 * oneBook()
   }
 }
 
 function allDifferentBooks(assortedHPBooks) {
   let allBooks = assortedHPBooks.length
   let uniqueBooks = unique(assortedHPBooks).length
-  if (allBooks == uniqueBooks) {
-    return true
-  }
+  return allBooks == uniqueBooks
+}
+
+function duplicativeBooks(assortedHPBooks) {
+  return unique(assortedHPBooks).length
 }
 
 function oneBook() {
@@ -55,18 +93,6 @@ function fourDifferentBooks() {
 function fiveDifferentBooks() {
   return (normalPrice * 5) - twentyFivePercentDiscount
 }
-
-function duplicativeBooks(assortedHPBooks) {
-  duplicatives = []
-  for (const [index, value] of assortedHPBooks.entries()) {
-    if (value == assortedHPBooks[index + 1] ) {
-      duplicatives.push(value)
-    }
-  }
-  return duplicatives.length
-}
-
-
 
 
 module.exports = cashRegister;
--- test/potter.test.js
+++ test/potter.test.js
@@ -6,26 +6,113 @@ test('one copy of a HP book costs the standard amount', () => {
 })
 
 test('two different copies of a HP book costs gives a 5% discount', () => {
-    let assortedHPBooks = ["Harry Potter and the Philosopher\'s Stone", "Harry Potter and the Chamber of Secrets"]
+    let assortedHPBooks = ["Harry Potter and the Philosopher\'s Stone",
+                          "Harry Potter and the Chamber of Secrets"]
   expect(cashRegister(assortedHPBooks)).toBe(15.2)
 })
 
+test('two of the same copies of a HP book costs the standard amount', () => {
+    let assortedHPBooks = ["Harry Potter and the Philosopher\'s Stone",
+                          "Harry Potter and the Philosopher\'s Stone"]
+  expect(cashRegister(assortedHPBooks)).toBe(16)
+})
+
 test('three different copies of a HP book costs gives a 10% discount', () => {
-    let assortedHPBooks = ["Harry Potter and the Philosopher\'s Stone", "Harry Potter and the Chamber of Secrets", "Harry Potter and the Prisoner of Azkaban"]
+    let assortedHPBooks = ["Harry Potter and the Philosopher\'s Stone",
+                          "Harry Potter and the Chamber of Secrets",
+                          "Harry Potter and the Prisoner of Azkaban"]
   expect(cashRegister(assortedHPBooks)).toBe(21.60)
 })
 
-test('three books with two different copies gives a 10% discount on two books', () => {
-    let assortedHPBooks = ["Harry Potter and the Philosopher\'s Stone", "Harry Potter and the Chamber of Secrets", "Harry Potter and the Chamber of Secrets"]
+test('three books with two different copies gives a 5% discount on two books', () => {
+    let assortedHPBooks = ["Harry Potter and the Philosopher\'s Stone",
+                            "Harry Potter and the Chamber of Secrets",
+                            "Harry Potter and the Chamber of Secrets"]
   expect(cashRegister(assortedHPBooks)).toBe(23.20)
 })
 
+test('three of the same copies of a HP book costs the standard amount', () => {
+  let assortedHPBooks = ["Harry Potter and the Philosopher\'s Stone",
+                        "Harry Potter and the Philosopher\'s Stone",
+                        "Harry Potter and the Philosopher\'s Stone"]
+    expect(cashRegister(assortedHPBooks)).toBe(24)
+})
+
 test('four different copies of a HP book costs gives a 20% discount', () => {
-    let assortedHPBooks = ["Harry Potter and the Philosopher\'s Stone", "Harry Potter and the Chamber of Secrets", "Harry Potter and the Prisoner of Azkaban", "Harry Potter and the Goblet of fire"]
+    let assortedHPBooks = ["Harry Potter and the Philosopher\'s Stone",
+                            "Harry Potter and the Chamber of Secrets",
+                            "Harry Potter and the Prisoner of Azkaban",
+                            "Harry Potter and the Goblet of fire"]
   expect(cashRegister(assortedHPBooks)).toBe(25.60)
 })
 
+test('four books with three different copies gives a 10% discount', () => {
+  let assortedHPBooks = ["Harry Potter and the Philosopher\'s Stone",
+                          "Harry Potter and the Chamber of Secrets",
+                          "Harry Potter and the Prisoner of Azkaban",
+                          "Harry Potter and the Prisoner of Azkaban"]
+  expect(cashRegister(assortedHPBooks)).toBe(29.60)
+})
+
+test('four books with two different copies gives a 5% discount', () => {
+  let assortedHPBooks = ["Harry Potter and the Philosopher\'s Stone",
+                         "Harry Potter and the Prisoner of Azkaban",
+                          "Harry Potter and the Prisoner of Azkaban",
+                          "Harry Potter and the Prisoner of Azkaban"]
+  expect(cashRegister(assortedHPBooks)).toBe(31.20)
+})
+
+test('four books of the same book gives the standard price', () => {
+  let assortedHPBooks = ["Harry Potter and the Prisoner of Azkaban",
+                         "Harry Potter and the Prisoner of Azkaban",
+                         "Harry Potter and the Prisoner of Azkaban",
+                          "Harry Potter and the Prisoner of Azkaban"]
+  expect(cashRegister(assortedHPBooks)).toBe(32.00)
+})
+
+
 test('five different copies of a HP book costs gives a 25% discount', () => {
-    let assortedHPBooks =  ["Harry Potter and the Philosopher\'s Stone", "Harry Potter and the Philosopher\'s Stone", "Harry Potter and the Prisoner of Azkaban", "Harry Potter and the Goblet of fire", "Order of the Phoenix"]
+    let assortedHPBooks =  ["Harry Potter and the Philosopher\'s Stone",
+                            "Harry Potter and the Chamber of Secrets",
+                            "Harry Potter and the Prisoner of Azkaban",
+                            "Harry Potter and the Goblet of fire",
+                            "Order of the Phoenix"]
   expect(cashRegister(assortedHPBooks)).toBe(30.00)
+})
+
+test('five books with 4 different HP books gives the 20% discount', () => {
+    let assortedHPBooks =  ["Harry Potter and the Philosopher\'s Stone",
+                            "Harry Potter and the Philosopher\'s Stone",
+                            "Harry Potter and the Prisoner of Azkaban",
+                            "Harry Potter and the Goblet of fire",
+                            "Order of the Phoenix"]
+  expect(cashRegister(assortedHPBooks)).toBe(33.60)
+})
+
+test('five books with 3 different HP books gives the 10% discount', () => {
+    let assortedHPBooks =  ["Harry Potter and the Philosopher\'s Stone",
+                            "Harry Potter and the Philosopher\'s Stone",
+                            "Harry Potter and the Philosopher\'s Stone",
+                            "Harry Potter and the Goblet of fire",
+                            "Order of the Phoenix"]
+  expect(cashRegister(assortedHPBooks)).toBe(37.60)
+})
+
+test('five books with 2 different HP books gives the 5% discount', () => {
+    let assortedHPBooks =  ["Harry Potter and the Philosopher\'s Stone",
+                            "Harry Potter and the Philosopher\'s Stone",
+                            "Harry Potter and the Philosopher\'s Stone",
+                            "Harry Potter and the Philosopher\'s Stone",
+                            "Order of the Phoenix"]
+  expect(cashRegister(assortedHPBooks)).toBe(39.20)
+})
+
+
+test('five books of the same titles gives the standard price', () => {
+    let assortedHPBooks =  ["Harry Potter and the Philosopher\'s Stone",
+                            "Harry Potter and the Philosopher\'s Stone",
+                            "Harry Potter and the Philosopher\'s Stone",
+                            "Harry Potter and the Philosopher\'s Stone",
+                            "Harry Potter and the Philosopher\'s Stone"]
+  expect(cashRegister(assortedHPBooks)).toBe(40.00)
 })
